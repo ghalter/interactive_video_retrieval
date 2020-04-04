b739c71cbff0778cc41da2d4aece83500d76334a
Gaudenz Halter <gaudenz.halter@live.com>
2020-04-04 08:17:24 +0200

    init flask app

---
+++ main.app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+import requests
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ offline_processing.py
@@ -0,0 +1,4 @@
+"""
+Here we preprocess all data, store it in some database / hdf5 file, for later retrieval
+
+"""
